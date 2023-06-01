11cf7656e56349867f38bd80af2ec23c679d7136
ash <ashkherad@gmail.com>
2023-06-01 11:54:37 -0400

    Change copy home.

--- src/components/MainBanner/MainBanner.jsx
+++ src/components/MainBanner/MainBanner.jsx
@@ -14,7 +14,7 @@ const MainBanner = () => (
             <p className="mb-6">
                 {/* This project is built with data from a MakeUp API using Axios, Tailwind CSS, ReactJS, PropTypes, React
                 Context API and React Router. */}
-                Allures is a ecommerce store that sells clothes, accessories, jewelery and electronics. We have a wide
+                Allures is an online store that sells clothes, accessories, jewelery and electronics. We have a wide
                 range of products for you can choose from. Shipping is always free!
             </p>
             <p className="mb-6">
